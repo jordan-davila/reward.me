066d5eabc0b46b86bc7e35ed6c0e8a20f7eb01de
Jordan Davila <hi@jordandavila.com>
2018-08-20 13:07:07 -0400

    Initial Commit

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\habit;
+use Illuminate\Http\Request;
+
+class HabitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, habit $habit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(habit $habit)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RewardController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\reward;
+use Illuminate\Http\Request;
+
+class RewardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function show(reward $reward)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(reward $reward)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, reward $reward)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\reward  $reward
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(reward $reward)
+    {
+        //
+    }
+}
